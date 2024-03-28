ab4403e3d1dbc6ba3853f3e5cd0773027f0ecc64
rustyrene <rene.delion.buisness@protonmail.com>
2024-03-28 18:00:37 +0100

    Add Connect/Disconnect Functionality

--- server/src/actors/lobby.rs
+++ server/src/actors/lobby.rs
@@ -1,8 +1,18 @@
+use std::collections::HashMap;
+
 use actix::prelude::*;
+use uuid::Uuid;
 
-use super::messages::{Connect, Disconnect};
+use super::{
+    chat_room::Room,
+    messages::{Connect, Disconnect},
+    ws_conn::ws_conn,
+};
 
-pub struct Lobby {}
+pub struct Lobby {
+    pub sessions: HashMap<Uuid, Addr<ws_conn>>,
+    pub chat_rooms: HashMap<Uuid, Addr<Room>>,
+}
 
 impl Actor for Lobby {
     type Context = Context<Self>;
@@ -11,15 +21,15 @@ impl Actor for Lobby {
 impl Handler<Disconnect> for Lobby {
     type Result = ();
 
-    fn handle(&mut self, _msg: Disconnect, _ctx: &mut Self::Context) {
-        println!("Disconnecting!");
+    fn handle(&mut self, msg: Disconnect, _ctx: &mut Self::Context) {
+        self.sessions.remove(&msg.user_id);
     }
 }
 
 impl Handler<Connect> for Lobby {
     type Result = ();
 
-    fn handle(&mut self, _msg: Connect, _ctx: &mut Self::Context) -> Self::Result {
-        println!("Connecting");
+    fn handle(&mut self, msg: Connect, _ctx: &mut Self::Context) -> Self::Result {
+        self.sessions.insert(msg.user_id, msg.user_addr);
     }
 }
