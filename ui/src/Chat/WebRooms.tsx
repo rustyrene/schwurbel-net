f6055d66c3cdf3e63fa14586723575f0b44e005a
rustyrene <rene.delion.buisness@protonmail.com>
2024-04-01 14:14:32 +0200

    Add Create Room Btn

--- ui/src/Chat/WebRooms.tsx
+++ ui/src/Chat/WebRooms.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { Button, Modal, Form } from "react-bootstrap";
 import { IoAdd } from "react-icons/io5";
 
 interface Props {
@@ -7,16 +9,26 @@ interface Props {
 }
 
 function WebRooms({ room_ids, joined_room, on_join}: Props) {
+  const [showModal, setShowModal] = useState(false);
+  const [createRoomName, setCreateRoomName] = useState("");
 
-  const handleJoin = (key: string) => {
-    on_join(key)
+  const handle_create = () => {
+    console.log(createRoomName);
+    setShowModal(false);
+    setCreateRoomName("");
+  }
+
+  const handle_close = () => {
+    setShowModal(false);
+    setCreateRoomName("");
   }
 
   return (
     <>
+      <div className="ms-2">
         <div className="list-group">
           {room_ids.length > 0 && room_ids.filter(room_id => room_id != "").map(room_id => 
-          <a href="#" className={`list-group-item list-group-item-action ${joined_room === room_id ? "active" : ""}`} key={room_id} onClick={() => handleJoin(room_id)}>
+          <a href="#" className={`list-group-item list-group-item-action ${joined_room === room_id ? "active" : ""}`} key={room_id} onClick={() => on_join(room_id)}>
             <div className="d-flex w-100 justify-content-between">
               <h5 className="mb-1">{room_id}</h5>
               <small><span className="badge text-bg-primary rounded-pill"></span></small>
@@ -25,8 +37,25 @@ function WebRooms({ room_ids, joined_room, on_join}: Props) {
           </a>)}
         </div>
         <div className="d-flex create-room-btn">
-          <button className="btn btn-primary ms-3"><IoAdd className="fs-3" /> Create Room</button>
+          <button className="btn btn-primary" onClick={() => setShowModal(true)}><IoAdd className="fs-3" /> Create Room</button>
         </div>
+        {/* Modal */}
+        <Modal show={showModal} onHide={handle_close} centered>
+          <Modal.Header closeButton>
+            <Modal.Title>Create a new </Modal.Title>
+           </Modal.Header>
+          <Modal.Body>
+            <Form.Group>
+              <Form.Label>Room Name</Form.Label>
+              <Form.Control type="text" value={createRoomName} onChange={(event) => setCreateRoomName(event.target.value)} />
+            </Form.Group>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handle_close}>Close</Button>
+            <Button variant="primary" onClick={handle_create}>Create</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
     </>
   )
 }
